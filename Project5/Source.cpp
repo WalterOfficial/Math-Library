af5f7547a48f55d07cfa06bb3a901e899ef2fbca
walterOfficial <55947289+WalterOfficial@users.noreply.github.com>
2026-01-05 15:00:19 +0700

    Изменена логика функций

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -21,37 +21,56 @@ double rad_to_deg( double n ) {
     return n * ( m_pi / m_pi_rad );
 }
 
-int main( ) {
-    setlocale( LC_ALL, "rus" );
-
-    cout << "Введите необходимый угол: ";
-
-    double x = 0.0;
-    cin >> x;
-
-    double sum = 0.0;
-    double sum2 = 0.0;
-
+__forceinline double normalize(double x) {
     x = fmod( x, 360.0 );
 
     if ( x > 180.0 ) x -= 360.0;
     if ( x < -180.0 ) x += 360.0;
 
-    x = deg_to_rad( x );
+    return x;
+}
 
-    for ( int i = 1; i <= 6; i++ ) {
+__forceinline double get_sin(double x) {
+    for ( int i = 1; i <= 5; i++ )
         sum += pow( -1, i - 1 ) * ( pow( x, 2 * i - 1 ) / factorial( 2 * i - 1 ) );
-    }
 
-    for ( int i = 0; i <= 5; i++ ) {
-        sum2 += pow( -1, i ) * ( pow( x, 2 * i ) / factorial( 2 * i ) );
-    }
+    const double eps = 1e-6;
+    if (fabs(sum) < eps)
+        return 0.0;
 
-    cout << setprecision( 2 ) << fixed;
-    cout << "sin: " << sum;
-    cout << "\ncos: " << sum2;
-    cout << "\ntan: " << ( sum / sum2 );
-    cout << "\ncotan: " << ( sum2 / sum );
+    return sum;
+}
+
+__forceinline double get_cos(double x) {
+    double sum = 0.0;
+    for ( int i = 0; i <= 5; i++ )
+        sum += pow( -1, i ) * ( pow( x, 2 * i ) / factorial( 2 * i ) );
+
+    const double eps = 1e-6;
+    if (fabs(sum) < eps)
+        return 0.0;
+
+    return sum;
+}
+
+int main( ) {
+    setlocale( LC_ALL, "rus" );
+
+    cout << "Введите необходимый угол: ";
+
+    // получаем необходимый угол
+    double x = 0.0;
+    cin >> x;
+
+    // ограничиваем угол в предел [-180, 180]
+    x = normalize( x );
+
+    // переводим угол в радианы для корректного расчёта
+    x = deg_to_rad( x );
+    
+    cout << "sin: " << get_sin( x );
+    cout << "\ncos: " << get_cos( x );
 
     return 0;
 }
+
