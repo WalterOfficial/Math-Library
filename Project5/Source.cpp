345293e9e01607b2ec16e3cb3e43e8cfaf3c98cb
walterOfficial <55947289+WalterOfficial@users.noreply.github.com>
2026-01-29 18:11:05 +0700

    fixed

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-#include "Math.h"
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
 constexpr double m_pi_rad = 3.1415926535;
 constexpr double m_pi = 180.0;
 
 double get_pow( double num, double powr ) {
-	double num2 = num;
+    double num2 = num;
 
-	if ( powr == 0 )
-		return 1;
+    if ( powr == 0 )
+        return 1;
 
-	for ( int i = 1; i < powr; i++ )
-		num2 = num2 * num;
+    for ( int i = 1; i < powr; i++ )
+        num2 = num2 * num;
 
-	auto lognum = log( num );
-	auto exp1 = exp( powr * lognum );
+    auto lognum = log( num );
+    auto exp1 = exp( powr * lognum );
 
-	return powr < 1.0 ? exp1 : num2;
+    return powr < 1.0 ? exp1 : num2;
 }
 
 double get_sqrt( double x, double sqrts ) {
-	return get_pow( x, 1.0 / sqrts );
+    return get_pow( x, 1.0 / sqrts );
 }
 
 int factorial( int n ) {
@@ -40,7 +40,7 @@ double rad_to_deg( double n ) {
     return n * ( m_pi / m_pi_rad );
 }
 
-__forceinline double normalize(double x) {
+double normalize( double x ) {
     x = fmod( x, 360.0 );
 
     if ( x > 180.0 ) x -= 360.0;
@@ -49,24 +49,26 @@ __forceinline double normalize(double x) {
     return x;
 }
 
-__forceinline double get_sin(double x) {
+double get_sin( double x ) {
+    double sum = 0.0;
+
     for ( int i = 1; i <= 5; i++ )
         sum += get_pow( -1, i - 1 ) * ( get_pow( x, 2 * i - 1 ) / factorial( 2 * i - 1 ) );
 
-    const double eps = 1e-6;
-    if (fabs(sum) < eps)
+    const double eps = 1e-3;
+    if ( fabs( sum ) < eps )
         return 0.0;
 
     return sum;
 }
 
-__forceinline double get_cos(double x) {
+double get_cos( double x ) {
     double sum = 0.0;
     for ( int i = 0; i <= 5; i++ )
         sum += get_pow( -1, i ) * ( get_pow( x, 2 * i ) / factorial( 2 * i ) );
 
-    const double eps = 1e-6;
-    if (fabs(sum) < eps)
+    const double eps = 1e-3;
+    if ( fabs( sum ) < eps )
         return 0.0;
 
     return sum;
@@ -86,11 +88,12 @@ int main( ) {
 
     // переводим угол в радианы для корректного расчёта
     x = deg_to_rad( x );
-    
+
+    cout << fixed;
     cout << "sin: " << get_sin( x );
-    cout << "\ncos: " << get_cos( x );
+    cout << "\ncos: " << get_cos( x ) << '\n';
+
+    system( "pause" );
 
     return 0;
 }
-
-
